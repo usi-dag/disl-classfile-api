e101b805c5f56b14e1e279378d0d5d6cea16b47f
alessiogiovagnini <alessio.giovagnini@usi.ch>
2025-02-12 16:22:56 +0100

    refactor of a marker

--- src-disl/ch/usi/dag/disl/marker/EmptyMarker.java
+++ src-disl/ch/usi/dag/disl/marker/EmptyMarker.java
@@ -1,5 +1,6 @@
 package ch.usi.dag.disl.marker;
 
+import java.lang.classfile.MethodModel;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -14,5 +15,10 @@ public class EmptyMarker extends AbstractMarker {
     @Override
     public List <MarkedRegion> mark (final MethodNode method) {
         return new LinkedList <MarkedRegion> ();
+    }
+
+    @Override
+    public List<MarkedRegion> mark(final MethodModel method) {
+        return new LinkedList<>();
     }
 }
