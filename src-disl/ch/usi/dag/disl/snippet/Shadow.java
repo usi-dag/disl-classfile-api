f9ae0a187b9078fddc812e5dcaced06d70143482
alessiogiovagnini <alessio.giovagnini@usi.ch>
2025-02-12 16:14:38 +0100

    refactored class Shadow.java

--- src-disl/ch/usi/dag/disl/snippet/Shadow.java
+++ src-disl/ch/usi/dag/disl/snippet/Shadow.java
@@ -1,10 +1,9 @@
 package ch.usi.dag.disl.snippet;
 
+import java.lang.classfile.ClassModel;
+import java.lang.classfile.CodeElement;
+import java.lang.classfile.MethodModel;
 import java.util.List;
-
-import org.objectweb.asm.tree.AbstractInsnNode;
-import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.MethodNode;
 
 /**
  * Holds information about a region where a snippet will be woven. The shadow
@@ -15,24 +14,21 @@ import org.objectweb.asm.tree.MethodNode;
  * should the code be woven.
  */
 public final class Shadow {
-
-    protected final ClassNode classNode;
-    protected final MethodNode methodNode;
+    protected final ClassModel classModel;
+    protected final MethodModel methodModel;
 
     protected final Snippet snippet;
 
-    private final AbstractInsnNode regionStart;
-    private final List <AbstractInsnNode> regionEnds;
+    private final CodeElement regionStart;
+    private final List<CodeElement> regionEnds;
 
     private final WeavingRegion weavingRegion;
-
 
     /**
      * Holds exact information where the code will be woven. This structure is
      * a guiding source for the weaver.
      */
     public static class WeavingRegion {
-
         // NOTE: "ends" can be null. This means, that we have the special case
         // where we need to generate before and after snippets on the same
         // position.
@@ -42,126 +38,54 @@ public final class Shadow {
         // instruction otherwise is never invoked.
         // "ends" containing null notifies the weaver about this situation.
 
-        private AbstractInsnNode start;
+        private CodeElement start;
+        private List<CodeElement> ends;
+        private CodeElement afterThrowStart;
+        private CodeElement afterThrowEnd;
 
-        private List<AbstractInsnNode> ends;
-
-        private AbstractInsnNode afterThrowStart;
-        private AbstractInsnNode afterThrowEnd;
-
-
-        public WeavingRegion (
-            final AbstractInsnNode start, final List <AbstractInsnNode> ends,
-            final AbstractInsnNode afterThrowStart, final AbstractInsnNode afterThrowEnd
-        ) {
-            super ();
+        public WeavingRegion(final CodeElement start, final List<CodeElement> ends,
+                             final CodeElement afterThrowStart, final CodeElement afterThrowEnd) {
+            super();
             this.start = start;
             this.ends = ends;
             this.afterThrowStart = afterThrowStart;
             this.afterThrowEnd = afterThrowEnd;
         }
 
+        public CodeElement getStart() {return start;}
+        public List<CodeElement> getEnds() {return ends;}
+        public CodeElement getAfterThrowStart() {return afterThrowStart;}
+        public CodeElement getAfterThrowEnd() {return afterThrowEnd;}
 
-        public AbstractInsnNode getStart() {
-            return start;
-        }
-
-        public List<AbstractInsnNode> getEnds() {
-            return ends;
-        }
-
-        public AbstractInsnNode getAfterThrowStart() {
-            return afterThrowStart;
-        }
-
-        public AbstractInsnNode getAfterThrowEnd() {
-            return afterThrowEnd;
-        }
-
-        public void setStart(final AbstractInsnNode start) {
-            this.start = start;
-        }
-
-        public void setEnds(final List<AbstractInsnNode> ends) {
-            this.ends = ends;
-        }
-
-        public void setAfterThrowStart(final AbstractInsnNode afterThrowStart) {
-            this.afterThrowStart = afterThrowStart;
-        }
-
-        public void setAfterThrowEnd(final AbstractInsnNode afterThrowEnd) {
-            this.afterThrowEnd = afterThrowEnd;
-        }
-
+        public void setStart(final CodeElement start) {this.start = start;}
+        public void setEnds(final List<CodeElement> ends) {this.ends = ends;}
+        public void setAfterThrowStart(CodeElement afterThrowStart) {this.afterThrowStart = afterThrowStart;}
+        public void setAfterThrowEnd(CodeElement afterThrowEnd) {this.afterThrowEnd = afterThrowEnd;}
     }
 
-
-    public Shadow (
-        final ClassNode classNode, final MethodNode methodNode, final Snippet snippet,
-        final AbstractInsnNode regionStart, final List <AbstractInsnNode> regionEnds,
-        final WeavingRegion weavingRegion
-    ) {
-        super ();
-        this.classNode = classNode;
-        this.methodNode = methodNode;
+    public Shadow(final ClassModel classModel, final MethodModel methodModel, final Snippet snippet,
+                  final CodeElement regionStart, final List<CodeElement> regionEnds, final WeavingRegion weavingRegion) {
+        super();
+        this.classModel = classModel;
+        this.methodModel = methodModel;
         this.snippet = snippet;
         this.regionStart = regionStart;
         this.regionEnds = regionEnds;
         this.weavingRegion = weavingRegion;
     }
-
-
     // special copy constructor for caching support
-    public Shadow (final Shadow that) {
-        this.classNode = that.classNode;
-        this.methodNode = that.methodNode;
+    public Shadow(final Shadow that) {
+        this.classModel = that.classModel;
+        this.methodModel = that.methodModel;
         this.snippet = that.snippet;
         this.regionStart = that.regionStart;
         this.regionEnds = that.regionEnds;
         this.weavingRegion = that.weavingRegion;
     }
 
-
-    /**
-     * Returns class node of the class where the shadow is defined.
-     */
-    public ClassNode getClassNode() {
-        return classNode;
-    }
-
-    /**
-     * Returns method node of the method where the shadow is defined.
-     */
-    public MethodNode getMethodNode() {
-        return methodNode;
-    }
-
-    /**
-     * Returns snippet that will be woven.
-     */
-    public Snippet getSnippet() {
-        return snippet;
-    }
-
-    /**
-     * Returns region start (this region is designed for static analysis).
-     */
-    public AbstractInsnNode getRegionStart() {
-        return regionStart;
-    }
-
-    /**
-     * Returns region ends (this region is designed for static analysis).
-     */
-    public List<AbstractInsnNode> getRegionEnds() {
-        return regionEnds;
-    }
-
-    /**
-     * Returns weaving region (this region is designed for weaver).
-     */
-    public WeavingRegion getWeavingRegion() {
-        return weavingRegion;
-    }
+    public ClassModel getClassModel() {return classModel;}
+    public MethodModel getMethodModel() {return methodModel;}
+    public CodeElement getRegionStart() {return regionStart;}
+    public List<CodeElement> getRegionEnds() {return regionEnds;}
+    public WeavingRegion getWeavingRegion() {return weavingRegion;}
 }
