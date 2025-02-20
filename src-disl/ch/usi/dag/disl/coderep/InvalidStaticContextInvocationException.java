e482a773f048d889573219a17279e684cd1f3a8b
alessiogiovagnini <alessio.giovagnini@usi.ch>
2025-02-20 15:16:42 +0100

    Refactored InvalidStaticContextInvocationException

--- src-disl/ch/usi/dag/disl/coderep/InvalidStaticContextInvocationException.java
+++ src-disl/ch/usi/dag/disl/coderep/InvalidStaticContextInvocationException.java
@@ -1,23 +1,24 @@
 package ch.usi.dag.disl.coderep;
 
-import org.objectweb.asm.tree.MethodInsnNode;
+
+import java.lang.classfile.instruction.InvokeInstruction;
 
 
 @SuppressWarnings ("serial")
 final class InvalidStaticContextInvocationException extends RuntimeException {
 
-    private final MethodInsnNode __insn;
+    private final InvokeInstruction __insn;
 
     //
 
     public InvalidStaticContextInvocationException (
-        final String message, final MethodInsnNode insn
+        final String message, final InvokeInstruction insn
     ) {
         super (message);
         __insn = insn;
     }
 
-    public MethodInsnNode insn () {
+    public InvokeInstruction insn () {
         return __insn;
     }
 }
