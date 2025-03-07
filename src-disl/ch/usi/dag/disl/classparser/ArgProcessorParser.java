66a859e9c6acf62995966c78d932b1b10c5ec425
alessiogiovagnini <alessio.giovagnini@usi.ch>
2025-03-07 10:59:38 +0100

    refactored the class, also need to be tested in depth

--- src-disl/ch/usi/dag/disl/classparser/ArgProcessorParser.java
+++ src-disl/ch/usi/dag/disl/classparser/ArgProcessorParser.java
@@ -1,5 +1,9 @@
 package ch.usi.dag.disl.classparser;
 
+import java.lang.classfile.ClassModel;
+import java.lang.classfile.MethodModel;
+import java.lang.classfile.TypeKind;
+import java.lang.constant.ClassDesc;
 import java.lang.reflect.Method;
 import java.util.EnumSet;
 import java.util.HashMap;
@@ -9,9 +13,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import org.objectweb.asm.Type;
-import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.MethodNode;
+import ch.usi.dag.disl.util.ClassFileHelper;
 
 import ch.usi.dag.disl.annotation.Guarded;
 import ch.usi.dag.disl.annotation.ProcessAlso;
@@ -23,7 +25,6 @@ import ch.usi.dag.disl.localvar.LocalVars;
 import ch.usi.dag.disl.processor.ArgProcessor;
 import ch.usi.dag.disl.processor.ArgProcessorKind;
 import ch.usi.dag.disl.processor.ArgProcessorMethod;
-import ch.usi.dag.disl.util.AsmHelper;
 import ch.usi.dag.disl.util.JavaNames;
 import ch.usi.dag.util.Strings;
 
@@ -32,168 +33,123 @@ class ArgProcessorParser extends AbstractParser {
     //
     // The key is an ASM type representing the class
     // where the argument processor was defined.
-    //
-    private final Map <Type, ArgProcessor> __processors = new HashMap <> ();
+    // TODO I replaced Type with ClassDesc since in this case Type represent the type of a class and not a method,
+    //  for methods there is MethodTypeDesc
+    private final Map <ClassDesc, ArgProcessor> __processors = new HashMap <> ();
 
 
-    public Map <Type, ArgProcessor> initProcessors (final LocalVars localVars) {
+    public Map <ClassDesc, ArgProcessor> initProcessors (final LocalVars localVars) {
         __processors.values ().stream ().unordered ()
             .forEach (processor -> processor.init (localVars));
 
         return __processors;
     }
 
-    //
 
     // NOTE: this method can be called many times
-    public void parse (final ClassNode dislClass) throws ParserException {
-        processLocalVars (dislClass);
+    public void parse(final ClassModel dislClass) throws ParserException {
+        processLocalVars(dislClass);
 
-        //
-
-        final String className = AsmHelper.typeName (dislClass);
-        final List <ArgProcessorMethod> methods = __parseMethods (dislClass);
+        final String className = ClassFileHelper.typeName(dislClass);
+        final List<ArgProcessorMethod> methods = __parseMethods(dislClass);
         if (methods.isEmpty ()) {
             throw new ParserException (
-                "argument processor %s has no methods!", className
+                    "argument processor %s has no methods!", className
             );
         }
 
-        //
-
-        __processors.put (
-            Type.getObjectType (dislClass.name),
-            new ArgProcessor (className, methods)
-        );
+        __processors.put(dislClass.thisClass().asSymbol(), new ArgProcessor(className, methods));
     }
 
 
-    private List <ArgProcessorMethod> __parseMethods (
-        final ClassNode disClass
-    ) throws ParserException {
+    private List<ArgProcessorMethod> __parseMethods(final ClassModel dislClass) throws ParserException {
         try {
-            final String className = AsmHelper.typeName (disClass);
-            return disClass.methods.parallelStream ().unordered ()
-                .filter (m -> !JavaNames.isConstructorName (m.name))
-                .filter (m -> !JavaNames.isInitializerName (m.name))
-                // Throws ParserRuntimeException.
-                .map (m -> __parseMethodWrapper (className, m))
-                .collect (Collectors.toList ());
-
+            final String className = ClassFileHelper.typeName(dislClass);
+            return dislClass.methods().parallelStream().unordered()
+                    .filter(m -> !JavaNames.isConstructorName(m.methodName().stringValue()))
+                    .filter(m -> !JavaNames.isInitializerName(m.methodName().stringValue()))
+                    .map(m -> __parseMethodWrapper(className, m))
+                    .collect(Collectors.toList());
         } catch (final ParserRuntimeException e) {
-            throw new ParserException (e.getMessage (), e.getCause ());
+            throw new ParserException(e.getMessage(), e.getCause());
         }
     }
 
 
-    private ArgProcessorMethod __parseMethodWrapper (
-        final String className, final MethodNode method
-    ) {
-        //
-        // Wrap all parser exceptions into ParserRuntimeException so that
-        // __parseMethod() can be called from a stream pipeline.
-        //
+    private ArgProcessorMethod __parseMethodWrapper(final String className, final MethodModel method) {
         try {
-            return __parseMethod (className, method);
-
+            return __parseMethod(className, method);
         } catch (final Exception e) {
-            e.printStackTrace ();
+            e.printStackTrace();
             throw new ParserRuntimeException (
-                e, "error parsing argument processor method %s.%s()",
-                className, method.name
+                    e, "error parsing argument processor method %s.%s()",
+                    className, method.methodName().stringValue()
             );
         }
     }
 
 
-    private ArgProcessorMethod __parseMethod (
-        final String className, final MethodNode method
-    ) throws ParserException, GuardException  {
-        __ensureArgProcessorIsWellDefined (method);
+    private ArgProcessorMethod __parseMethod(final String className, final MethodModel method) throws ParserException, GuardException {
+        __ensureArgProcessorIsWellDefined(method);
 
-        //
         // Parse method annotation and ensure that additional parameter types
         // specified in the @ProcessAlso annotation are valid.
-        //
-        final AnnotationData data = AnnotationData.parse (method);
-        final ArgProcessorKind defaultType = ArgProcessorKind.forMethod (method);
-        __ensureValidProcessAlsoTypes (defaultType, data.processAlsoType);
+        final AnnotationData data = AnnotationData.parse(method);
+        final ArgProcessorKind defaultType = ArgProcessorKind.forMethod(method);
+        __ensureValidProcessAlsoTypes(defaultType, data.processAlsoType);
 
-        //
         // Determine the set of parameter types accepted by the processor.
-        //
-        final Set <ArgProcessorKind> processedTypes = EnumSet.of (defaultType);
-        processedTypes.addAll (data.processAlsoType);
+        final Set<ArgProcessorKind> processedTypes = EnumSet.of(defaultType);
+        processedTypes.addAll(data.processAlsoType);
 
-        //
         // Lookup the guard method in the specified guard class.
         // TODO LB: Consider moving method resolution to annotation processing.
-        //
-        final Method guardMethod = GuardHelper.findAndValidateGuardMethod (
-            data.guardClass.orElse (null), GuardHelper.processorContextSet ()
-        );
+        final Method guardMethod = GuardHelper.findAndValidateGuardMethod(data.guardClass.orElse(null), GuardHelper.processorContextSet());
 
-        //
         // Determine the kinds of contexts used in the processor parameters,
         // and create a code template for the argument processor method.
-        //
-        final UnprocessedCode codeTemplate = new UnprocessedCode (
-            className, method
-        );
+        final UnprocessedCode codeTemplate = new UnprocessedCode(className, method);
 
-        return new ArgProcessorMethod (
-            processedTypes, guardMethod, codeTemplate
-        );
+        return new ArgProcessorMethod(processedTypes, guardMethod, codeTemplate);
     }
 
 
-    private void __ensureArgProcessorIsWellDefined (
-        final MethodNode method
-    ) throws ParserException {
-        //
-        // The ordering of (some of) these checks is important -- some may
-        // rely on certain assumptions to be satisfied.
-        //
-        __ensureMethodHasAtLeastOneParameter (method);
-        __ensureFirstParameterIsObjectOrPrimitive (method);
+    private void __ensureArgProcessorIsWellDefined(final MethodModel method) throws ParserException {
+        __ensureMethodHasAtLeastOneParameter(method);
+        __ensureFirstParameterIsObjectOrPrimitive(method);
 
-        ensureMethodIsStatic (method);
-        ensureMethodReturnsVoid (method);
-        ensureMethodThrowsNoExceptions (method);
+        ensureMethodIsStatic(method);
+        ensureMethodReturnsVoid(method);
+        ensureMethodThrowsNoExceptions(method);
 
-        ensureMethodIsNotEmpty (method);
-        ensureMethodUsesContextProperly (method);
+        ensureMethodIsNotEmpty(method);
+        ensureMethodUsesContextProperly(method);
     }
 
 
-    private static void __ensureMethodHasAtLeastOneParameter (
-        final MethodNode method
-    ) throws ParserException {
-        if (Type.getArgumentTypes (method.desc).length < 1) {
+    private static void __ensureMethodHasAtLeastOneParameter(final MethodModel methodModel) throws ParserException {
+        if (methodModel.methodTypeSymbol().parameterCount() < 1) {
             throw new ParserException ("method has no parameters!");
         }
     }
 
 
-    private static final Type __OBJECT_TYPE__= Type.getType (Object.class);
-
-    private static void __ensureFirstParameterIsObjectOrPrimitive (
-        final MethodNode method
-    ) throws ParserException {
-        final Type argType = Type.getArgumentTypes (method.desc) [0];
-        final int sort = argType.getSort ();
-
-        if (Type.VOID < sort && sort <= Type.ARRAY) {
-            return;
+    private static void __ensureFirstParameterIsObjectOrPrimitive(final MethodModel method) throws ParserException {
+        ClassDesc argType = method.methodTypeSymbol().parameterType(0);
+        TypeKind typeKind = TypeKind.fromDescriptor(argType.descriptorString());
+        // TODO is this equivalent to the asm version???
+        if (argType.isPrimitive() && !typeKind.equals(TypeKind.VoidType)) {
+            return; // primitive are accepted except void
         }
-
-        if (Type.OBJECT == sort && __OBJECT_TYPE__.equals (argType)) {
-            return;
+        if (argType.isArray()) {
+            return; // array are accepted
         }
-
+        if (argType.descriptorString().equals(Object.class.descriptorString())) {
+            return; // type Object is accepted
+        }
         throw new ParserException (
-            "type of the first argument must be Object or a non-void "+
-            "primitive type, found %s!", argType.getClassName ()
+                "type of the first argument must be Object or a non-void "+
+                        "primitive type, found %s!", argType.displayName()
         );
     }
 
@@ -213,35 +169,36 @@ class ArgProcessorParser extends AbstractParser {
 
         //
 
-        public static AnnotationData parse (final MethodNode method) {
-            final AnnotationData result = new AnnotationData ();
+        public static AnnotationData parse(final MethodModel method) {
+            final AnnotationData result = new AnnotationData();
 
-            new AnnotationMapper ()
-                .register (Guarded.class, "guard", (n, v) -> {
-                    if (v instanceof Type) {
-                        result.guardClass = Optional.of (__resolveClass ((Type) v));
-                    }
-                })
-                .register (ProcessAlso.class, "types", (n, v) -> {
-                    if (v instanceof List <?>) {
-                        @SuppressWarnings ("unchecked")
-                        final List <ProcessAlso.Type> values = (List <ProcessAlso.Type>) v;
-                        result.processAlsoType = __toArgProcessorKindSet (values);
-                    }
-                })
-                .processDefaults ()
-                .accept (method);
+            new AnnotationMapper()
+                    .register(Guarded.class, "guard", (n, v) -> {
+                        if (v instanceof ClassDesc) {
+                            result.guardClass = Optional.of(__resolveClass((ClassDesc) v));
+                        }
+                    })
+                    .register(ProcessAlso.class, "types", (n, v) -> {
+                        if (v instanceof List<?>) {
+                            // TODO is this correct ???????????
+                            @SuppressWarnings ("unchecked")
+                            final List <ProcessAlso.Type> values = (List <ProcessAlso.Type>) v;
+                            result.processAlsoType = __toArgProcessorKindSet (values);
+                        }
+                    })
+                    .processDefaults()
+                    .accept(method);
 
             return result;
         }
 
 
-        private static Class <?> __resolveClass (final Type type) {
-            try {
-                return Class.forName (type.getClassName ());
 
+        private static Class<?> __resolveClass(final ClassDesc classDesc) {
+            try {
+                return Class.forName(classDesc.displayName());
             } catch (final ClassNotFoundException e) {
-                throw new ParserRuntimeException (e);
+                throw new ParserRuntimeException(e);
             }
         }
 
