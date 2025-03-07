c16b3ed5d53eb87954ea6a2bc7b72275d39f1a96
alessiogiovagnini <alessio.giovagnini@usi.ch>
2025-03-07 11:01:40 +0100

    refactored the class, need to verify the function of the mapper, I left some TODO in places to double check

--- src-disl/ch/usi/dag/disl/classparser/AnnotationMapper.java
+++ src-disl/ch/usi/dag/disl/classparser/AnnotationMapper.java
@@ -4,8 +4,6 @@ import java.lang.classfile.Annotation;
 import java.lang.classfile.AnnotationElement;
 import java.lang.classfile.AnnotationValue;
 import java.lang.classfile.MethodModel;
-import java.lang.classfile.attribute.RuntimeInvisibleAnnotationsAttribute;
-import java.lang.classfile.attribute.RuntimeVisibleAnnotationsAttribute;
 import java.lang.classfile.constantpool.Utf8Entry;
 import java.lang.constant.ClassDesc;
 import java.lang.reflect.Method;
@@ -13,14 +11,9 @@ import java.util.*;
 import java.util.function.BiConsumer;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 import ch.usi.dag.disl.util.ClassFileHelper;
-// TODO remove all asm instances once the other are verified that works correctly
-import org.objectweb.asm.AnnotationVisitor;
-import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.Type;
-import org.objectweb.asm.tree.AnnotationNode;
-import org.objectweb.asm.tree.MethodNode;
 
 
 final class AnnotationMapper {
@@ -98,63 +91,16 @@ final class AnnotationMapper {
     }
 
 
-    public AnnotationMapper accept (final MethodNode mn) {
-        Arrays.asList (mn.visibleAnnotations, mn.invisibleAnnotations).stream ()
-            .filter (l -> l != null)
-            .flatMap (l -> l.stream ())
-            .forEach (an -> accept (an));
-
-        return this;
-    }
-
     public AnnotationMapper accept(final MethodModel methodModel) {
-        List<RuntimeInvisibleAnnotationsAttribute> runtimeInvisibleAnnotationsAttributes = ClassFileHelper.getInvisibleAnnotation(methodModel);
-        List<RuntimeVisibleAnnotationsAttribute> runtimeVisibleAnnotationsAttributes = ClassFileHelper.getVisibleAnnotation(methodModel);
-        List<Annotation> visibleAnnotation = runtimeVisibleAnnotationsAttributes.stream()
-                .map(RuntimeVisibleAnnotationsAttribute::annotations)
-                .flatMap(Collection::stream)
-                .toList();
-        List<Annotation> invisibleAnnotation = runtimeInvisibleAnnotationsAttributes.stream()
-                .map(RuntimeInvisibleAnnotationsAttribute::annotations)
-                .flatMap(Collection::stream)
-                .toList();
-        Arrays.asList(visibleAnnotation, invisibleAnnotation).stream()
+        List<Annotation> visibleAnnotation = ClassFileHelper.getVisibleAnnotation(methodModel);
+        List<Annotation> invisibleAnnotation = ClassFileHelper.getInvisibleAnnotation(methodModel);
+        Stream.of(visibleAnnotation, invisibleAnnotation)
                 .flatMap(Collection::stream)
                 .forEach(this::accept);
 
         return this;
     }
 
-    public AnnotationMapper accept (final AnnotationNode an) {
-        final Class <?> ac = __resolveClass (Type.getType (an.desc));
-
-        final Map <
-            Predicate <String>, BiConsumer <String, Object>
-        > consumers = __findConsumers (ac);
-
-        an.accept (new AnnotationVisitor (Opcodes.ASM9) {
-            @Override
-            public void visit (final String name, final Object value) {
-                __getConsumer (consumers, name).accept (name, value);
-            }
-
-            @Override
-            public void visitEnum (
-                final String name, final String desc, final String value
-            ) {
-                final Object enumValue = __instantiateEnum (desc, value);
-                __getConsumer (consumers, name).accept (name, enumValue);
-            }
-
-            @Override
-            public AnnotationVisitor visitArray (final String name) {
-                final BiConsumer <String, Object> consumer = __getConsumer (consumers, name);
-                return new ListCollector (name, consumer);
-            }
-        });
-
-        return this;
-    }
 
     public AnnotationMapper accept(final Annotation annotation) {
         final Class<?> ac = __resolveClass(annotation.classSymbol());
@@ -179,6 +125,7 @@ final class AnnotationMapper {
                     acceptElement(elementName,innerAnnotationValue, consumers);
                 }
             }
+
             case AnnotationValue.OfEnum ofEnum -> {
                 // TODO is ofEnum.constantName() equivalent to "Object value" of method "Visit" from asm visitor????
                 final Object enumValue = __instantiateEnum(ofEnum.classSymbol().descriptorString(), ofEnum.constantName().stringValue());
@@ -202,52 +149,6 @@ final class AnnotationMapper {
         }
     }
 
-
-    /**
-     * Collects individual values into a list and submits the result to the
-     * given consumer when the {@link #visitEnd()} method is called.
-     * <p>
-     * <b>Note:</b>This collector does not currently support nested arrays or
-     * annotation values.
-     */
-    private static class ListCollector extends AnnotationVisitor {
-        final List <Object> __values = new ArrayList <> ();
-
-        final String __name;
-        final BiConsumer <String, Object> __consumer;
-
-        ListCollector (final String name, final BiConsumer <String, Object> consumer) {
-            super (Opcodes.ASM9);
-
-            __name = name;
-            __consumer = consumer;
-        }
-
-        @Override
-        public void visit (final String name, final Object value) {
-            __values.add (value);
-        }
-
-        @Override
-        public void visitEnum (final String name, final String desc, final String value) {
-            __values.add (__instantiateEnum (desc, value));
-        }
-
-        @Override
-        public void visitEnd () {
-            __consumer.accept (__name, __values);
-        }
-    };
-
-
-    private static Class <?> __resolveClass (final Type type) {
-        try {
-            return Class.forName (type.getClassName ());
-
-        } catch (final ClassNotFoundException e) {
-            throw new ParserRuntimeException (e);
-        }
-    }
 
     private static Class<?> __resolveClass(final ClassDesc desc) {
         try {
