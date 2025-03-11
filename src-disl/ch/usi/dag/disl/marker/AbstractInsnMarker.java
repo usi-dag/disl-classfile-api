4b62de85ae20485d2806f42a56ffd58a67202c24
alessiogiovagnini <alessio.giovagnini@usi.ch>
2025-03-11 15:04:23 +0100

    marker refactored, for one inheritor class InsnNodeMarker it might not be possible to refactor it

--- src-disl/ch/usi/dag/disl/marker/AbstractInsnMarker.java
+++ src-disl/ch/usi/dag/disl/marker/AbstractInsnMarker.java
@@ -5,33 +5,11 @@ import java.lang.classfile.MethodModel;
 import java.util.LinkedList;
 import java.util.List;
 
-import org.objectweb.asm.tree.AbstractInsnNode;
-import org.objectweb.asm.tree.MethodNode;
-
 import ch.usi.dag.disl.snippet.Shadow.WeavingRegion;
 
 
 abstract class AbstractInsnMarker extends AbstractMarker {
 
-    @Override
-    public final List <MarkedRegion> mark (final MethodNode methodNode) {
-        final List <MarkedRegion> regions = new LinkedList <MarkedRegion> ();
-
-        for (final AbstractInsnNode instruction : markInstruction (methodNode)) {
-            final MarkedRegion region = new MarkedRegion (instruction, instruction);
-            region.setWeavingRegion (new WeavingRegion (
-                instruction, new LinkedList <AbstractInsnNode> (region.getEnds ()),
-                instruction, instruction
-            ));
-
-            regions.add (region);
-        }
-
-        return regions;
-    }
-
-
-    public abstract List <AbstractInsnNode> markInstruction (MethodNode methodNode);
 
     public abstract List<CodeElement> markInstruction(MethodModel methodModel);
 
