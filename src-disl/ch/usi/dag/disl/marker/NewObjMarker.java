135167914fa160987cae5f42a506198bb237a10a
alessiogiovagnini <alessio.giovagnini@usi.ch>
2025-03-11 14:14:44 +0100

    marker refactored

--- src-disl/ch/usi/dag/disl/marker/NewObjMarker.java
+++ src-disl/ch/usi/dag/disl/marker/NewObjMarker.java
@@ -1,14 +1,10 @@
 package ch.usi.dag.disl.marker;
 
+import java.lang.classfile.*;
+import java.lang.classfile.instruction.InvokeInstruction;
 import java.util.LinkedList;
 import java.util.List;
 
-import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.tree.AbstractInsnNode;
-import org.objectweb.asm.tree.MethodInsnNode;
-import org.objectweb.asm.tree.MethodNode;
-
-import ch.usi.dag.disl.util.AsmHelper.Insns;
 import ch.usi.dag.disl.util.JavaNames;
 
 /**
@@ -22,30 +18,31 @@ import ch.usi.dag.disl.util.JavaNames;
 public class NewObjMarker extends AbstractDWRMarker {
 
     // NOTE: does not work for arrays
-
     @Override
-    public List<MarkedRegion> markWithDefaultWeavingReg(MethodNode method) {
-
-        List<MarkedRegion> regions = new LinkedList<MarkedRegion>();
+    public List<MarkedRegion> markWithDefaultWeavingReg(MethodModel methodModel) {
+        List<MarkedRegion> regions = new LinkedList<>();
         int invokedNews = 0;
 
+        if (methodModel.code().isEmpty()) {
+            return regions;
+        }
+
         // find invocation of constructor after new instruction
-        for (AbstractInsnNode instruction : Insns.selectAll (method.instructions)) {
-
+        List<CodeElement> instructions = methodModel.code().get().elementList();
+        for (CodeElement codeElement: instructions) {
+            if (!(codeElement instanceof Instruction instruction)) {
+                continue;
+            }
             // track new instruction
-            if (instruction.getOpcode() == Opcodes.NEW) {
-
+            if (instruction.opcode() == Opcode.NEW) {
                 ++invokedNews;
             }
+            // if it is invokespecial and there are new pending
+            if (instruction.opcode() == Opcode.INVOKESPECIAL && invokedNews > 0) {
+                InvokeInstruction invokeInstruction = (InvokeInstruction) instruction;
 
-            // if it is invoke special and there are new pending
-            if (instruction.getOpcode() == Opcodes.INVOKESPECIAL
-                    && invokedNews > 0) {
-
-                MethodInsnNode min = (MethodInsnNode) instruction;
-
-                if (JavaNames.isConstructorName (min.name)) {
-                    regions.add(new MarkedRegion(instruction, instruction));
+                if (JavaNames.isConstructorName(invokeInstruction.name().stringValue())) {
+                    regions.add(new MarkedRegion(codeElement, codeElement));
                 }
             }
         }
