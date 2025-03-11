931e49021eb07dd33ddf15d13da41795e727e3d2
alessiogiovagnini <alessio.giovagnini@usi.ch>
2025-03-11 14:41:16 +0100

    marker refactored

--- src-disl/ch/usi/dag/disl/marker/EmptyMarker.java
+++ src-disl/ch/usi/dag/disl/marker/EmptyMarker.java
@@ -4,18 +4,11 @@ import java.lang.classfile.MethodModel;
 import java.util.LinkedList;
 import java.util.List;
 
-import org.objectweb.asm.tree.MethodNode;
-
 
 /**
  * Marker does not create any marking.
  */
 public class EmptyMarker extends AbstractMarker {
-
-    @Override
-    public List <MarkedRegion> mark (final MethodNode method) {
-        return new LinkedList <MarkedRegion> ();
-    }
 
     @Override
     public List<MarkedRegion> mark(final MethodModel method) {
