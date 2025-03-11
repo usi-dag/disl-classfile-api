51982d7c80bd25578043ac6f5bce444a3ed2e8be
alessiogiovagnini <alessio.giovagnini@usi.ch>
2025-03-11 14:45:04 +0100

    marker refactored

--- src-disl/ch/usi/dag/disl/marker/BodyMarker.java
+++ src-disl/ch/usi/dag/disl/marker/BodyMarker.java
@@ -1,14 +1,12 @@
 package ch.usi.dag.disl.marker;
 
+import java.lang.classfile.CodeElement;
+import java.lang.classfile.MethodModel;
+import java.lang.classfile.instruction.ReturnInstruction;
 import java.util.LinkedList;
 import java.util.List;
 
-import org.objectweb.asm.tree.AbstractInsnNode;
-import org.objectweb.asm.tree.MethodNode;
-
 import ch.usi.dag.disl.snippet.Shadow.WeavingRegion;
-import ch.usi.dag.disl.util.AsmHelper;
-import ch.usi.dag.disl.util.AsmHelper.Insns;
 
 
 /**
@@ -19,23 +17,24 @@ import ch.usi.dag.disl.util.AsmHelper.Insns;
  */
 public class BodyMarker extends AbstractMarker {
 
-    @Override
-    public List <MarkedRegion> mark (final MethodNode method) {
-        final List <MarkedRegion> regions = new LinkedList <MarkedRegion> ();
-        final MarkedRegion region = new MarkedRegion (
-            method.instructions.getFirst ()
-        );
 
-        for (final AbstractInsnNode insn : Insns.selectAll (method.instructions)) {
-            if (AsmHelper.isReturn (insn)) {
-                region.addEnd (insn);
+    @Override
+    public List<MarkedRegion> mark(final MethodModel method) {
+        final List<MarkedRegion> regions = new LinkedList<>();
+        if (method.code().isEmpty()) {
+            return regions; // TODO should I return an error maybe???
+        }
+        List<CodeElement> instructions = method.code().get().elementList();
+        final MarkedRegion region = new MarkedRegion(instructions.getFirst());
+        for (final CodeElement codeElement: instructions) {
+            if (codeElement instanceof ReturnInstruction) {
+                region.addEnd(codeElement);
             }
         }
-
-        final WeavingRegion wregion = region.computeDefaultWeavingRegion (method);
-        wregion.setAfterThrowEnd (method.instructions.getLast ());
-        region.setWeavingRegion (wregion);
-        regions.add (region);
+        final WeavingRegion weavingRegion = region.computeDefaultWeavingRegion(method);
+        weavingRegion.setAfterThrowEnd(instructions.getLast());
+        region.setWeavingRegion(weavingRegion);
+        regions.add(region);
         return regions;
     }
 
