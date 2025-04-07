e39135bfc8c54bdca615f26d1be883d638759d1f
alessiogiovagnini <alessio.giovagnini@usi.ch>
2025-04-07 09:52:48 +0200

    refactored even if deprecated

--- src-disl/ch/usi/dag/disl/staticcontext/BytecodeStaticContext.java
+++ src-disl/ch/usi/dag/disl/staticcontext/BytecodeStaticContext.java
@@ -1,5 +1,8 @@
 package ch.usi.dag.disl.staticcontext;
 
+import java.lang.classfile.CodeElement;
+import java.lang.classfile.Instruction;
+
 /**
  * Provides static context information about instrumented bytecode.
  * <p>
@@ -13,7 +16,12 @@ public class BytecodeStaticContext extends AbstractStaticContext {
 	 * Returns (ASM) integer number of the instrumented bytecode.
 	 */
 	public int getBytecodeNumber() {
-
-		return staticContextData.getRegionStart().getOpcode();
+		CodeElement element = staticContextData.getRegionStart();
+		if (element instanceof Instruction instruction) {
+			return instruction.opcode().bytecode();
+		} else {
+			// this is to replicate the ASM behaviour when an element is pseudo instruction
+			return -1;
+		}
 	}
 }
