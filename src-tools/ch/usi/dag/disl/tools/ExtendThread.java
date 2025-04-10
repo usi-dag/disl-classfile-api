45b44acf5ecc317013d96fbabc8dec75ef99a22b
alessiogiovagnini <alessio.giovagnini@usi.ch>
2025-04-10 11:37:50 +0200

    refactored this class to use ClassFile

--- src-tools/ch/usi/dag/disl/tools/ExtendThread.java
+++ src-tools/ch/usi/dag/disl/tools/ExtendThread.java
@@ -3,14 +3,10 @@ package ch.usi.dag.disl.tools;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-
-import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassVisitor;
-import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.MethodVisitor;
-import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.Type;
-import org.objectweb.asm.commons.AdviceAdapter;
+import java.io.InputStream;
+import java.lang.classfile.*;
+import java.lang.reflect.AccessFlag;
+import static java.lang.constant.ConstantDescs.CD_boolean;
 
 
 /**
@@ -19,97 +15,6 @@ import org.objectweb.asm.commons.AdviceAdapter;
  * code, which checks the state of the "bypass" variable.
  */
 public final class ExtendThread {
-
-    private static abstract class FieldDefinition {
-
-        private final int __access;
-        private final String __name;
-        private final Class <?> __type;
-
-
-        FieldDefinition (
-            final int access, final String name, final Class <?> type
-        ) {
-            __access = access;
-            __name = name;
-            __type = type;
-        }
-
-        int access () {
-            return __access;
-        }
-
-        String name () {
-            return __name;
-        }
-
-        String desc () {
-            return Type.getDescriptor (__type);
-        }
-
-        Type type () {
-            return Type.getType (__type);
-        }
-
-        protected abstract void initialize (Type owner, AdviceAdapter mv);
-    }
-
-
-    private static final class FieldExtender extends ClassVisitor {
-        private final FieldDefinition __fd;
-        private Type __owner;
-
-        public FieldExtender (final FieldDefinition fd, final ClassWriter cw) {
-            super (Opcodes.ASM9, cw);
-            __fd = fd;
-        }
-
-        @Override
-        public void visit (
-            final int version, final int access, final String name,
-            final String signature, final String superName,
-            final String [] interfaces
-        ) {
-            super.visit (version, access, name, signature, superName, interfaces);
-            __owner = Type.getObjectType (name);
-        };
-
-
-        @Override
-        public MethodVisitor visitMethod (
-            final int access, final String name, final String desc,
-            final String signature, final String [] exceptions
-        ) {
-            final MethodVisitor mv = super.visitMethod (
-                access, name, desc, signature, exceptions
-            );
-
-            if ("<init>".equals (name)) {
-                // Initialize the field.
-                return new AdviceAdapter (Opcodes.ASM9, mv, access, name, desc) {
-                    @Override
-                    protected void onMethodEnter () {
-                        __fd.initialize (__owner, this);
-                    }
-                };
-            } else {
-                return mv;
-            }
-        };
-
-
-        @Override
-        public void visitEnd () {
-            // Create the field.
-            super.visitField (
-                __fd.access (), __fd.name (), __fd.desc (),
-                null, null
-            );
-
-            super.visitEnd ();
-        }
-    }
-
 
     public static void main (final String... args) throws Exception {
         if (args.length < 1) {
@@ -124,34 +29,47 @@ public final class ExtendThread {
             System.exit (1);
         }
 
-        //
-        // Define a thread-local non-inheritable boolean variable named
-        // "bypass", with a default value of false,
-        //
-        final FieldDefinition bypassField = new FieldDefinition (
-            Opcodes.ACC_PUBLIC, "bypass", boolean.class) {
-            @Override
-            protected void initialize (final Type owner, final AdviceAdapter mv) {
-                mv.loadThis ();
-                mv.push (false);
-                mv.putField (owner, name (), type ());
-            }
-        };
 
         //
         // Extend Thread and dump the new bytecode into the given directory.
         //
-        final byte [] extendedThread = __extendThread (bypassField);
+        final byte [] extendedThread = __extendThread();
         __writeThread (outputDir, extendedThread);
     }
 
 
-    private static byte [] __extendThread (final FieldDefinition fd)
-    throws IOException {
-        final ClassReader cr = new ClassReader (Thread.class.getName ());
-        final ClassWriter cw = new ClassWriter (cr, 0);
-        cr.accept (new FieldExtender (fd, cw), ClassReader.EXPAND_FRAMES);
-        return cw.toByteArray ();
+    private static byte[] __extendThread() throws IOException {
+        InputStream inputStream = ClassLoader.getSystemResourceAsStream(Thread.class.getName().replace(".", "/") + ".class");
+        if (inputStream == null) {
+            throw new RuntimeException("Cannot find Thread class. Error in ExtendThread.java");
+        }
+        byte[] thread = inputStream.readAllBytes();
+        final ClassModel classModel = ClassFile.of().parse(thread);
+        return ClassFile.of().build(classModel.thisClass().asSymbol(), classBuilder -> {
+            for (ClassElement classElement: classModel) {
+                // add the initialization to all the constructors
+                if (classElement instanceof MethodModel  methodModel && methodModel.methodName().equalsString("<init>")) {
+                    classBuilder.transformMethod(methodModel, (methodBuilder, methodElement) -> {
+                        if (methodElement instanceof CodeModel codeModel) {
+                            methodBuilder.withCode(codeBuilder -> {
+                                codeBuilder.aload(0); // load this
+                                codeBuilder.bipush(0);  // load false
+                                codeBuilder.putfield(classModel.thisClass().asSymbol(), "bypass", CD_boolean);
+                                for (CodeElement element: codeModel) {
+                                    codeBuilder.with(element);
+                                }
+                            });
+                        } else {
+                            methodBuilder.with(methodElement);
+                        }
+                    });
+                } else {
+                    classBuilder.with(classElement);
+                }
+            }
+            // add the field
+            classBuilder.withField("bypass", CD_boolean, AccessFlag.PUBLIC.mask());
+        });
     }
 
 
@@ -163,7 +81,7 @@ public final class ExtendThread {
         final String dirName = pkgName.replace ('.', File.separatorChar);
 
         final File outputDir = new File (baseDir, dirName);
-        outputDir.mkdirs ();
+        final boolean wasMade = outputDir.mkdirs();
 
         final String fileName = String.format ("%s.class", tc.getSimpleName ());
         final File outputFile = new File (outputDir, fileName);
